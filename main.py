7c914dad1e781c3a8b09088926942206a16b27cd
Calvin Aw <caw@stuy.edu>
2019-02-18 22:13:49 -0500

    ok

--- main.py
+++ main.py
@@ -5,7 +5,35 @@ from matrix import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
+print "adding (1,2,3), (4,5,6) to matrix m via edge"
+add_edge(matrix,1,2,3,4,5,6)
+add_point(matrix,1,5)
+print_matrix(matrix)
+
+print "testing identity"
+matrix2 = new_matrix()
+ident(matrix2)
+print_matrix(matrix2)
+
+print "testing multiplication of m1 (inverse) * m"
+matrix_mult(matrix2,matrix)
+print_matrix(matrix)
+
+print "testing multiplication of m3 (with 1,1,1 on last 2 col and 0 on rest) * m"
+matrix3 = new_matrix(4,2)
+add_edge(matrix3,1,1,1,1,1,1)
+matrix_mult(matrix3,matrix)
+print_matrix(matrix)
+
+drawmatrix = new_matrix(4,0)
+add_edge(drawmatrix,100,200,300,400,500,600)
 
 
-draw_lines( matrix, screen, color )
+i = 0
+while (i < 100):
+    draw_lines( drawmatrix, screen, color )
+    drawmatrix[0][1] += 10
+    drawmatrix[1][0] -= 1
+    i+=1
+    
 display(screen)
--- matrix.py
+++ matrix.py
@@ -15,23 +15,22 @@ def print_matrix( matrix ):
     r = 0
     while r < len(matrix):
         for i in matrix[r]:
-            print i
-            print " "
-        print "\n"
+            print i,
+            print " ",
+        print "\n",
         r += 1
 
 #turn the paramter matrix into an identity matrix
 #you may assume matrix is square
 def ident( matrix ):
     r = 0
-    c = 0
     while r < len(matrix):
         c = 0
         while c < len(matrix):
             if (r == c):
-                matrix[r][c] = 0
-            else:
                 matrix[r][c] = 1
+            else:
+                matrix[r][c] = 0
             c += 1
         r+=1
 
@@ -46,6 +45,7 @@ def matrix_mult( m1, m2 ):
     res = 0
     List = []
     while col_m2 < len(m2[0]):  #changing m2 column
+        List = []
         row_m1 = 0
         while row_m1 < len(m1): #resetting m1 row
             #for m1, row_m1 is same
@@ -55,22 +55,23 @@ def matrix_mult( m1, m2 ):
             res = 0
             #computes result of col_m2
             while (col_m1 < len(m1) and row_m2 < len(m2)): 
-                res += m1[row_1][col_1] * m2[row_2][col_2]
+                res += m1[row_m1][col_m1] * m2[row_m2][col_m2]
                 col_m1 += 1
                 row_m2 += 1
             List.append(res)
-            col_m2 += 1 #move on to others
             row_m1 += 1
-        int k = 0
+        k = 0
         for i in List:
             m2[k][col_m2] = i
             k+=1
+        col_m2 += 1 #move on to others
+
 
 
 def new_matrix(rows = 4, cols = 4):
     m = []
-    for c in range( cols ):
+    for r in range( rows ):
         m.append( [] )
-        for r in range( rows ):
-            m[c].append( 0 )
+        for c in range( cols ):
+            m[r].append( 0 )
     return m
